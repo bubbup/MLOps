57c073e42e0f1091a8fa1f62c71ed0561d82b126
Noor Akram <N.Akram2204@nu.edu.eg>
2025-10-16 17:22:08 +0300

    ran experiment 2 logistic regression

--- train.py
+++ train.py
@@ -1,15 +1,16 @@
 import os
 import pandas as pd
 import joblib
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
 from sklearn.metrics import accuracy_score
 
 train_data= pd.read_csv('data/train.csv')
 X = train_data.drop('target', axis=1)
 y = train_data['target']
 
-model = RandomForestClassifier(
-    n_estimators=100,
+model = LogisticRegression(
+    max_iter=1000,
     random_state=42
 )
 model.fit(X, y)
@@ -17,5 +18,5 @@ model.fit(X, y)
 train_preds = model.predict(X)
 train_acc = accuracy_score(y, train_preds)
 
-model_path = os.path.join('models', 'RFmodel.pkl')
+model_path = os.path.join('models', 'model.pkl')
 joblib.dump(model, model_path)
