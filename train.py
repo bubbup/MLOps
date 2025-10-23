758b7f3df9697780ab4fa9b63dd8f66882338c1a
Noor Akram <N.Akram2204@nu.edu.eg>
2025-10-23 16:33:11 +0300

    trained an svm

--- train.py
+++ train.py
@@ -1,22 +1,23 @@
 import os
 import pandas as pd
 import joblib
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
-from sklearn.metrics import accuracy_score
-
-train_data= pd.read_csv('data/train.csv')
+# Load training data
+train_data = pd.read_csv('data/train.csv')
 X = train_data.drop('target', axis=1)
 y = train_data['target']
 
-model = LogisticRegression(
-    max_iter=1000,
+# Define and train SVM model
+model = SVC(
+    C=1.0,               # Regularization parameter
+    kernel='rbf',        # Kernel type
+    probability=True,    # Enable probabilities for later validation
     random_state=42
 )
 model.fit(X, y)
 
-train_preds = model.predict(X)
-train_acc = accuracy_score(y, train_preds)
-
+# Save trained model
+os.makedirs('models', exist_ok=True)
 model_path = os.path.join('models', 'model.pkl')
 joblib.dump(model, model_path)
